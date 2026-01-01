0db9c428a0c4a0878d2c8864f9bbd5adc59e7bb5
laidaiann <laibigann@gmail.com>
2026-01-01 21:58:56 +0800

    修版管理介面的程式資訊

--- admin/about.php
+++ admin/about.php
@@ -16,34 +16,14 @@ $label_color = array("default", "primary", "info", "danger", "success");
 ?>
 <div class="jumbotron">
 	<div class="container">
-		<h1>感謝您的使用</h1>
-		<?php
-		// [Fix] Check if aboutData is valid object
-		if (is_object($aboutData)) {
-			echo "<hr>";
-			echo "<p>" . htmlspecialchars($aboutData->msg) . "</p>";
-			echo "<hr>";
-			echo "<p class=\"small_font\">最新版本：" . htmlspecialchars($aboutData->latest_version);
-			if ($aboutData->latest_version != $config_current_version) {
-				// [Fix] Fixed broken string concatenation and quoting
-				echo "<a href=\"" . htmlspecialchars($aboutData->project_link) . "\" target=\"_blank\" class=\"label label-warning margin_box\">前往下載最新版本</a>";
-			}
-			echo "<p class=\"small_font\">更新日期：" . htmlspecialchars($aboutData->latest_update) . "</p>";
-			echo "<p class=\"small_font\">作者：" . htmlspecialchars($aboutData->author) . "</p>";
-			$count = 0;
-			if (isset($aboutData->links) && is_object($aboutData->links)) {
-				foreach ($aboutData->links as $linkKey => $linkVal) {
-					// [Fix] Ensure index exists and avoid out of bounds
-					$color = $label_color[$count] ?? 'default';
-					echo "<a class=\"btn btn-" . $color . " btn-lg margin_box small_font\" href=\"" . htmlspecialchars($linkVal) . "\" target=\"_blank\">" . htmlspecialchars($linkKey) . "</a>";
-					$count++;
-					if ($count >= count($label_color))
-						$count = 0; // wrap around color
-				}
-			}
-		} else {
-			echo "<p>無法載入遠端資訊，請稍後再試。</p>";
-		}
-		?>
+		<h1>龍潭國中公告系統</h1>
+		<hr>
+		<p>這是龍潭國中的公告管理系統。</p>
+		<p>源自 C.P.Sub V5.3 公告系統</p>
+		<p>如有任何問題，請聯繫資訊組。</p>
+		<p>分機 214 @ 龍潭國中</p>
+		<hr>
+		<p class="small_font">版本：v1.0</p>
+		<p class="small_font">維護單位：資訊組</p>
 	</div>
 </div>
